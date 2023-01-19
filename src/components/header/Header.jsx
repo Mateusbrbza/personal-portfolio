d36972789e7d494630a570853207f33fd9d3b791
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-19 00:08:07 -0300

    beginning header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <nav className="nav container">
+            <a href="index.html" className="nav__logo">Mateus</a>
+
+            <div className="nav__menu">
+                <ul className="nav__list grid">
+                    <li className="nav__item">
+                        <a href="#home" className="nav__link">
+                            <i className="uil uil-estate nav__icon"></i> 
+                            Home
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#about" className="nav__link">
+                            <i className="uil uil-user nav__icon"></i> 
+                            Sobre
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#skills" className="nav__link">
+                            <i className="uil uil-file nav__icon"></i> 
+                            Habilidades
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#services" className="nav__link">
+                            <i className="uil uil-briefcase nav__icon"></i> 
+                            Servicos
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#portfolio" className="nav__link">
+                            <i className="uil uil-scenery nav__icon"></i> 
+                            Portfolio
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#contact" className="nav__link">
+                            <i className="uil uil-message nav__icon"></i> 
+                            Contato
+                        </a>
+                    </li>
+                </ul>
+
+                <i class="uil uil-times nav__close" ></i>
+            </div>
+
+            <div className="nav__toggle">
+                <i class="uil uil-apps"></i>
+            </div>
+        </nav>
+    </header>
   )
 }
 
