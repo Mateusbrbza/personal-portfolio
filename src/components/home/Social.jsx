2499187bc6b42f1b2beb0c5bd42973dcd46e74ba
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-20 21:29:38 -0300

    Home components

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/mateusbrbza/" 
+        className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/mateus-barboza-70a443229/" 
+        className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/Mateusbrbza" 
+        className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
