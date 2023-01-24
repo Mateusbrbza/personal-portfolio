85f6cd76fc7ce7adea7f15e01daf16e88c849e10
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-23 23:33:22 -0300

    about concluido

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info grid">
+        <div className="about__box">
+        <i class='bx bx-award about__icon'></i>
+
+            <h3 className="about__title">Experiencia</h3>
+            <span className="about__subtitle">inserir informacao</span>
+        </div>
+
+        <div className="about__box">
+        <i class='bx bx-briefcase about__icon' ></i>
+
+            <h3 className="about__title">Numero de projetos</h3>
+            <span className="about__subtitle">inserir informacoes</span>
+        </div>
+
+        <div className="about__box">
+        <i class='bx bx-support about__icon' ></i>
+
+            <h3 className="about__title">Disponibilidade para contato</h3>
+            <span className="about__subtitle">inserir informacoes</span>
+        </div>
+    </div>
   )
 }
 
