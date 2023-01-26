c202de4fc0504a977593d7ac3830059725e95606
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-25 21:59:13 -0300

    Updating global classes

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Habilidades</h2>
+      <span className="section__subtitle">Nível Técnico</span>
+    </section>
   )
 }
 
-export default Skills
+export default Skills;
