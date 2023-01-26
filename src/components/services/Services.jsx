21f3db6f9965e05f3c9e44cea982cec22c585836
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-25 23:44:56 -0300

    iniciando services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,6 +5,30 @@ const Services = () => {
     <section className="services section" id="services">
         <h2 className="section__title">Serviços</h2>
         <span className="section__subtitle">O que eu ofereço</span>
+
+        <div className="services__container container grid">
+            <div>
+                <i className="uil uil services__icon"></i>
+                <h3 className="services__title"></h3>
+            </div>
+
+            <span className="services__button">
+            Ver Mais 
+            <i className="uil uil-arrow-right services button-icon"></i>
+            </span>
+
+            <div className="services__modal">
+                <div className="services__modal-content">
+                    <i className="uil uil-times services__modal-close"></i>
+
+                    <h3 className="services__modal-title"></h3>
+                    <p className="services__modal-description">
+                        Providenciando trabalho de qualidade 
+                        para clientes e empresas.
+                    </p>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
