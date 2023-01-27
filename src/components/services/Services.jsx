e6b7a290ba976fdd877fd530143272751395c661
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-26 21:52:37 -0300

    incluindo servicos

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -8,12 +8,14 @@ const Services = () => {
 
         <div className="services__container container grid">
             <div>
-                <i className="uil uil services__icon"></i>
-                <h3 className="services__title"></h3>
+                <i className="uil uil-web-grid services__icon"></i>
+                <h3 className="services__title"
+                >Designer <br /> de produto
+                </h3>
             </div>
 
             <span className="services__button">
-            Ver Mais 
+            Ver Mais {" "}
             <i className="uil uil-arrow-right services button-icon"></i>
             </span>
 
@@ -26,6 +28,150 @@ const Services = () => {
                         Providenciando trabalho de qualidade 
                         para clientes e empresas.
                     </p>
+
+                    <ul className="services__modal-services grid">
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de interfaces para o usuário.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de páginas Web.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de aplicativos Mobile.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Faço o Design de experiência do usuário.
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div className="services__container container grid">
+            <div>
+                <i className="uil uil-arrow services__icon"></i>
+                <h3 className="services__title">
+                    Desenvolvedor <br /> Frontend
+                </h3>
+            </div>
+
+            <span className="services__button">
+           Ver Mais {" "} 
+            <i className="uil uil-arrow-right services button-icon"></i>
+            </span>
+
+            <div className="services__modal">
+                <div className="services__modal-content">
+                    <i className="uil uil-times services__modal-close"></i>
+
+                    <h3 className="services__modal-title"></h3>
+                    <p className="services__modal-description">
+                        Providenciando trabalho de qualidade 
+                        para clientes e empresas.
+                    </p>
+
+                    <ul className="services__modal-services grid">
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de interfaces para o usuário.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de páginas Web.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de aplicativos Mobile.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Faço o Design de experiência do usuário.
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        {/* definirposteriormente o que inserir */}
+        <div className="services__container container grid">
+            <div>
+                <i className="uil uil-edit services__icon"></i>
+                <h3 className="services__title">
+                    A definir <br />
+                    item.
+                </h3>
+            </div>
+
+            <span className="services__button">
+            Ver Mais {" "}
+            <i className="uil uil-arrow-right services button-icon"></i>
+            </span>
+
+            <div className="services__modal">
+                <div className="services__modal-content">
+                    <i className="uil uil-times services__modal-close"></i>
+
+                    <h3 className="services__modal-title"></h3>
+                    <p className="services__modal-description">
+                        Providenciando trabalho de qualidade 
+                        para clientes e empresas.
+                    </p>
+
+                    <ul className="services__modal-services grid">
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de interfaces para o usuário.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de páginas Web.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Desenvolvimento de aplicativos Mobile.
+                            </p>
+                        </li>
+
+                        <li className="services__modal-service">
+                            <i className="uil uil-check-circle services__modal-icon"></i>
+                            <p className="services__modal-info">
+                                Faço o Design de experiência do usuário.
+                            </p>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
