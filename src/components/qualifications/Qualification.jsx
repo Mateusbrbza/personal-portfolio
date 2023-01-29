f86ffb69f40d38ae67270769ec643c61ceffbc7d
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-01-29 18:37:39 -0300

    iniciando aba de qualificações

--- src/components/qualifications/Qualification.jsx
+++ src/components/qualifications/Qualification.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import "./qualification.css";
 
 const Qualification = () => {
   return (
-    <div>Qualification</div>
+    <section className="qualification section">
+      <h2 className="section__title">Qualificações</h2>
+      <span className="section__subtitle">Minha jornada</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div className="qualification__button button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Formação
+          </div>
+
+          <div className="qualification__button button--flex">
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experiência
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
