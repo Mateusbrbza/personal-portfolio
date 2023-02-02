b58e64f48334c3c69d06cbf4f28f4607c8dcf6b4
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-02-02 18:13:28 -0300

    .

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -7,28 +7,28 @@ import work6 from "../../assets/work6.png";
 export const projectsData = [
     {
         id: 1,
-        image: work1,
+        image: work6,
         title: 'Portfolio',
         link: 'https://portfolio-mateus-example.netlify.app',
         category: 'web',
     },
     {
         id: 1,
-        image: work1,
+        image: work3,
         title: 'e-gym',
         link: 'https://exercism-gym.netlify.app',
         category: 'web',
     },
     {
         id: 1,
-        image: work1,
+        image: work2,
         title: 'Admin Dashboard',
         link: 'https://adm-dashboard.netlify.app',
         category: 'web',
     },
     {
         id: 1,
-        image: work1,
+        image: work5,
         title: 'Netflix Clone',
         link: 'https://reactflix-clone.netlify.app',
         category: 'web',
