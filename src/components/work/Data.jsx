e86b21523ac6e1e7851277b4326a233b1560f959
mateusbrbza <mateus.c.barboza@gmail.com>
2023-05-24 10:01:13 -0300

    updated projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,31 +1,29 @@
-import work2 from "../../assets/work2.jpeg";
-import work3 from "../../assets/work3.jpeg";
 import work4 from "../../assets/work4.jpeg";
 import work5 from "../../assets/work5.jpeg"
-import work6 from "../../assets/work6.png";
 import work7 from "../../assets/work7.png";
+import messenger from "../../assets/messenger.gif";
 
 export const projectsData = [
     {
         id: 1,
-        image: work4,
-        title: 'REST API (Express, Auth, MongoDB)',
-        link: 'https://github.com/Mateusbrbza/REST-API',
-        category: 'backend'
-    },
-    {
-        id: 1,
         image: work7,
-        title: 'Twitter (Next, Auth, Prisma, Mongo)',
+        title: 'Twitter Clone',
         link: 'https://twitter-demo-kappa.vercel.app',
         category: 'frontend',
     },
     {
         id: 1,
-        image: work3,
-        title: 'e-gym',
-        link: 'https://exercism-gym.netlify.app',
+        image: messenger,
+        title: 'Live Messenger',
+        link: 'https://messenger-dun-one.vercel.app',
         category: 'frontend'
+    },
+    {
+        id: 1,
+        image: work4,
+        title: 'REST API with Express',
+        link: 'https://github.com/Mateusbrbza/REST-API',
+        category: 'backend'
     },
     {
         id: 1,
@@ -33,20 +31,6 @@ export const projectsData = [
         title: 'Netflix Clone',
         link: 'https://reactflix-clone.netlify.app',
         category: 'frontend', 
-    },
-    {
-        id: 1,
-        image: work2,
-        title: 'Admin Dashboard',
-        link: 'https://adm-dashboard.netlify.app',
-        category: 'frontend'
-    },
-    {
-        id: 1,
-        image: work6,
-        title: 'Ecommerce',
-        link: 'https://encantali-prototype.netlify.app',
-        category: 'frontend'
     },
 ];
 
